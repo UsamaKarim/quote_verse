213a1d8c99ce69a3af898b983ec92f974e7fea94
Usama Karim <sonuosama@gmail.com>
2025-09-06 21:33:08 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/quote/verse/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.verygoodcore.quote_verse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:quote_verse/app/app.dart';
+import 'package:quote_verse/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
